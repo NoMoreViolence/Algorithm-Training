e3b09107ee7f0bfc9e4ef3a06cf2fb15811bbbb6
NoMoreViolence <ljh86029926@gmail.com>
2018-02-28 10:52:36 +0900

    2108 수들의 합 5

--- C/2108/Sum.c
+++ C/2108/Sum.c
@@ -1,27 +1,22 @@
 // https://www.acmicpc.net/problem/2018     수들의 합: 2108
-
-// (i+1) + (i+2) + ... + (i+n) = input
-// 이걸 바꾸면
-// (1 + 2 + ... + n) + i * n = input
-// 밑의 식으로 된다면 가능한 것이다
-// (input - (1 + 2 + ... + n) ) % n == 0
-
 #include <stdio.h>
-
 int main()
 {
-    int input,
-        answer = 0,
-        i,
-        j,
-        n;
+    int input,      // 수들의 합을 구할 인풋값
+        answer = 0, // 수들의 합의 개수
+        i,          // for
+        j;          // for
 
     scanf("%d", &input);
 
+    // (i+1) + (i+2) + ... + (i+n) = input
+    // (1 + 2 + ... + n) + i * n = input
     for (i = 1, j = 0; (j += i) <= input; i++)
     {
+        // (input - (1 + 2 + ... + n) ) % n == 0
         if ((input - j) % i == 0)
         {
+            // 조건성립
             answer++;
         }
     }
